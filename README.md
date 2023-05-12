f3ddc085811eca77dea019ea4bf5777d038f1098
kakajun <38216933+kakajun@users.noreply.github.com>
2023-05-12 22:01:21 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nuxt3-test2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-c7uya9)
---
+++ nuxt.config.ts
@@ -0,0 +1,4 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  buildModules: ["@vueuse/nuxt", "@pinia/nuxt"],
+})
